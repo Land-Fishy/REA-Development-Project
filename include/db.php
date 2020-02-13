8604bd11448003c3363f62af94a8f60471d4b3b2
cool cat <rapeb0ya@hotmail.com>
2020-02-13 13:49:55 +0100

    db bestand toegevoegd

---
+++ include/db.php
@@ -0,0 +1,12 @@
+<?php
+
+try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=lewiswolfe;charset=utf8;', 'root','');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'connection gemaakt :)';
+    die;
+}catch(PDOException $e){
+    echo $e->getMessage().'<br>';
+    file_put_contents('connectionErrors.txt', $e->getMessage()."\n", FILE_APPEND);
+    die;
+}
