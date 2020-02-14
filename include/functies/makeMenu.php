5b03d1e9f6652b2dc954e2e90bec72dcd6d9fd78
cool cat <rapeb0ya@hotmail.com>
2020-02-14 13:30:34 +0100

    makeMenu fix

--- include/functies/makeMenu.php
+++ include/functies/makeMenu.php
@@ -1,13 +1,32 @@
 <?php
-function makeMenu($db, $class = false){
+function makeMenu($db, $pagename, $class =  "navbar-nav mr-auto mt-2 mt-lg-0"){
     $menu = getMenu($db);
-
     $html = '<ul'.($class ? ' class="'.$class.'">' : '>');
+    
     foreach($menu as $v){
-        $html .= '<li><a href="'.(makeLink($v['slug'])).'">'.$v['item'].'</a></li>';
+        if($v['item'] == $pagename){
+            $html .= '<li class="nav-item active">';
+            $html .= '<a class="nav-link" href="'.(makeLink($v['slug'])).'">'.$v['item'].'<span class="sr-only">(current)</span></a>';
+        }else{
+            $html .= '<li class="nav-item">';
+            $html .= '<a class="nav-link" href="'.(makeLink($v['slug'])).'">'.$v['item'].'</a>';
+        }
+        $html .= '</li>';
+        //        $html .= '<li><a href="'.(makeLink($v['slug'])).'">'.$v['item'].'</a></li>';
     }
-    $html .= '<ul>';
+    $html .= '</ul>';
     return $html;
 }
+
+// reference
+//     <li class="nav-item active">
+//       <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link" href="#">Link</a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link disabled" href="#">Disabled</a>
+//     </li>
 
 
--- include/functietester.php
+++ include/functietester.php
@@ -2,5 +2,7 @@
 include './db.php';
 include './functies/functies.php';
 
-echo listAmazonBooks($db,1);
+$current = $_GET['page'] ?? 'home';
+
+echo makeMenu($db, $current,  "navbar-nav mr-auto mt-2 mt-lg-0");
 
