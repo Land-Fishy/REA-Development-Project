dc74290cbc824a96d5b31bfad211b2d2503af566
Jermain <jermain@aboveinfinity.com>
2020-02-14 15:27:19 +0100

    + added cool fade feature, + Latest book release (Made a start)

--- include/header.php
+++ include/header.php
@@ -1,2 +1,9 @@
-<div class="header">
+<div class="header-image"></div>
+<div class="fade">
+	<div class="latest">
+		<img src="images/newbook.jpg">
+	</div>
+	<div class="latest-text">
+		
+	</div>
 </div>
--- include/nav.php
+++ include/nav.php
@@ -6,7 +6,7 @@
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Home </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
