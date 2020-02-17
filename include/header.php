af382d4d4f1cfcdbdcf6b2609b66cc51f9221ce0
Jermain <jermain@aboveinfinity.com>
2020-02-17 10:34:14 +0100

    + Added Font candara, + Latest HTML Layout

--- include/header.php
+++ include/header.php
@@ -1,9 +1,13 @@
 <div class="header-image"></div>
-<div class="fade">
+<div class="fad">
 	<div class="latest">
 		<img src="images/newbook.jpg">
 	</div>
 	<div class="latest-text">
-		
+		<h1>Wolfe's latest</h1>
+		<div class="latest-box">
+			<p>lorem ipsum</p>
+		</div>
+		<a href="#">get on amazon now!</a>
 	</div>
 </div>
