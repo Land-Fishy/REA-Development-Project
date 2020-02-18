62224af0575e4e40f98b410508f36eb4a21d45f8
Wieke Lemmens <wlemmens@reacollege.eu>
2020-02-18 15:39:07 +0100

    source image added

--- include/nav.php
+++ include/nav.php
@@ -1,8 +1,18 @@
 <nav>
-  <ul>
-    <li><a href="#">home</a></li>
-    <li><a href="#">books</a></li>
-    <li><a href="#">about me</a></li>
-    <li><a href="#">contact</a></li>
-  </ul>
+	<i class="fas fa-bars"></i>
+	<ul>
+		<li><a href="#" class="active">home</a></li>
+		<li><a href="#">books</a>
+			<ul class="dropdown">
+				<li><a href="#">Book 1</a></li>
+				<li><a href="#">Book 2</a></li>
+				<li><a href="#">Book 3</a></li>
+				<li><a href="#">Book 4</a></li>
+				<li><a href="#">Book 5</a></li>
+				<li><a href="#">Book 6</a></li>
+			</ul>
+		</li>
+		<li><a href="#">about me</a></li>
+		<li><a href="#">contact</a></li>
+	</ul>
 </nav>
