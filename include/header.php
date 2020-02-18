b0bdaa58491849b14a6656531dc54a7196e439ed
Wieke Lemmens <wlemmens@reacollege.eu>
2020-02-18 16:31:16 +0100

    source image added

--- include/header.php
+++ include/header.php
@@ -1,5 +1,5 @@
-<header>
-    <h1>Lewis Wolfe</h1>
+<header> 
+	<h1>Lewis Wolfe</h1>
 	<?php require_once 'include/nav.php';?>
 	<div class="triangle"></div>
 	<div class="gradient"></div>
--- index.php
+++ index.php
@@ -6,17 +6,17 @@ include 'include/db.php';
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/stylesheet.css" important>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <title>Lewis Wolfe</title>
   </head>
   <body>
     <div class="container">
-      <?php require_once 'include/header.php';?>
-      <main>
+		<?php require_once 'include/header.php';?>
+		<main>
 			
-      </main>
+		</main>
       <?php require_once 'include/footer.php';?>
     </div>
   </body>
