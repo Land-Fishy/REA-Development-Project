691d802088e92bc9a2cc77ab543d57be0da05d14
Wieke Lemmens <wlemmens@reacollege.eu>
2020-02-18 17:04:19 +0100

    source image added

--- include/header.php
+++ include/header.php
@@ -3,4 +3,11 @@
 	<?php require_once 'include/nav.php';?>
 	<div class="triangle"></div>
 	<div class="gradient"></div>
+	<div class="arrow">
+		<div></div>
+		<svg width="140" height="80">
+			<polygon points="0,0 70,70 140,0 140,80 0,80" style="fill:rgb(238,238,238)" />
+		</svg>
+		<div></div>
+	</div>
 </header>
