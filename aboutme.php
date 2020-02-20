9332c05f758a956c2f71e551585bc42ddd2a992c
Jermain <jermain@aboveinfinity.com>
2020-02-20 12:14:40 +0100

    flex not worky

---
+++ aboutme.php
@@ -0,0 +1,29 @@
+<?php
+include 'include/db.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css" important>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <title>Lewis Wolfe</title>
+  </head>
+  <body>
+    <div class="container">
+    <?php require_once 'include/header.php';?>
+    <main>
+      <div class="content">
+        <h1 class="title">lewis wolfe</h1>
+       <div class="about-me">
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div>
+       <img src="images/sexy.jpg">
+    </div>
+    </main>
+      <?php require_once 'include/footer.php';?>
+    </div>
+  </body>
+</html>
