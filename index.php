9438a85461cc6d7d3115d378507038f839c7ce2c
Wieke Lemmens <wlemmens@reacollege.eu>
2020-02-20 12:55:52 +0100

    css

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include 'include/db.php';
 ?>
 <!doctype html>
-<html lang="en">
+<html>
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,31 +12,29 @@ include 'include/db.php';
     <title>Lewis Wolfe</title>
   </head>
   <body>
-    <div class="container">
+    <div class="container" id="index">
 		<?php require_once 'include/header.php';?>
 		<main>
-      <div class="main">
-			 <figure>
-        <a href="#">
-          <img src="images/books/newbook.jpg">
-          <figcaption>a monster escapes</figcaption>
-        </a>
-      </figure>
-      <figure>
-          <img src="images/books/newbook.jpg">
-          <figcaption>a monster escapes</figcaption>
-      </figure>
-        <figure>
-          <img src="images/books/newbook.jpg">
-          <figcaption>a monster escapes</figcaption>
-      </figure>
-      <figure>
-        <img src="images/books/newbook.jpg">
-        <figcaption>a monster escapes</figcaption>
-      </figure>
-    </div>
+			<figure>
+				<a href="#">
+					<img src="images/books/newbook.jpg">
+					<figcaption>a monster escapes</figcaption>
+				</a>
+			</figure>
+			<figure>
+			  <img src="images/books/newbook.jpg">
+			  <figcaption>a monster escapes</figcaption>
+			</figure>
+			<figure>
+				<img src="images/books/newbook.jpg">
+				<figcaption>a monster escapes</figcaption>
+			</figure>
+			<figure>
+				<img src="images/books/newbook.jpg">
+				<figcaption>a monster escapes		</figcaption>
+			</figure>
 		</main>
-      <?php require_once 'include/footer.php';?>
+		<?php require_once 'include/footer.php';?>
     </div>
   </body>
 </html>
