e3ba30574dc125f7dfa5ea75206fcac6aa4180b2
Wieke Lemmens <wlemmens@reacollege.eu>
2020-02-20 17:14:00 +0100

    css cms

--- include/functies/makeBookImgLink.php
+++ include/functies/makeBookImgLink.php
@@ -4,6 +4,6 @@ function makeBookImgLink($img){
     if($_SERVER['HTTP_HOST'] == 'rea.loc'){
         return 'http://rea.loc/REA-Development-Project/images/books/'.$img;
     }else{
-        return 'http://localhost/rea-development-Project/images/books/'.$img;
+        return 'http://localhost/WolfeAlpha/images/books/'.$img;
     }
 }
