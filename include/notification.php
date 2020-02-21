3e4bd13af8a59e2abe2f81bdd71767b213f0d1ba
Casper Sloof <csloof@reacollege.eu>
2020-02-21 12:33:05 +0100

    popup balk toegevoegd

--- include/notification.php
+++ include/notification.php
@@ -1,5 +1,5 @@
 <section class="cookie-bar">
-	<p>We use cookies to give you the best experience of our website. By browsing you agree to our use of cookies.
-	<a a="#" id="cookie-understand">I understand!</a>
-	</p>
+	<p>We use cookies to give you the best experience of our website. By browsing you agree to our use of cookies.</p>
+	<a id="cookie-understand">I understand!</a>
 </section>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
--- script/botbar.js
+++ script/botbar.js
@@ -1,5 +1,40 @@
+// Button ID
+
+var buttonId = '#cookie-understand';
+
+// Cookie name
+
+var cookieName = 'website-cookie';
+
+// Cookie value
+
+var cookieValue = 'website-cookie-agree';
+
+// Cookie expire (days)
+
+var cookieExpire = 10;
+
+// When click button, create cookie.
+
 $(document).ready(function(){
-  $('#cookie-understand').click(function() {
-    $('.cookie-bar').hide();
+  $(buttonId).click(function() {
+    if ($.cookie(cookieName) == null){
+      // Create the cookie.
+      $.cookie(cookieName, cookieValue, { expires: cookieExpire, path: '/' });
+      // Hide the alert message.
+      $('.cookie-bar').hide();
+    }
   });
-}); 
+  checkCookie();
+});
+
+// If no cookie, show the cookie bar.
+
+function checkCookie(){
+    if ($.cookie(cookieName) == null) {
+      // No cookie = Show cookie bar
+      $('.cookie-bar').show();
+    } else {
+      $('.cookie-bar').hide();
+    }
+}
