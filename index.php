391b13ed5e20a0c45dd368c76ad2dcb7264b9daa
cool cat <rapeb0ya@hotmail.com>
2020-02-21 13:29:35 +0100

    kaas

--- index.php
+++ index.php
@@ -30,26 +30,10 @@ if(true){
   <body>
     <div class="container" id="index">
 		<?php require_once 'include/header.php';?>
-		<main>
-			<figure>
-				<a href="#">
-					<img src="images/books/newbook.jpg">
-					<figcaption>a monster escapes</figcaption>
-				</a>
-			</figure>
-			<figure>
-			  <img src="images/books/newbook.jpg">
-			  <figcaption>a monster escapes</figcaption>
-			</figure>
-			<figure>
-				<img src="images/books/newbook.jpg">
-				<figcaption>a monster escapes</figcaption>
-			</figure>
-			<figure>
-				<img src="images/books/newbook.jpg">
-				<figcaption>a monster escapes		</figcaption>
-			</figure>
-		</main>
+		  <main>
+        <?php echo makeBookFigures($db, $bookids); ?>
+
+		  </main>
 		<?php require_once 'include/notification.php';?>
 		<?php require_once 'include/footer.php';?>
     </div>
