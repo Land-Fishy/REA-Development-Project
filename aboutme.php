59ce7c8335819b8a24c09f1855b02ec2b95955f5
Jermain <jermain@aboveinfinity.com>
2020-02-21 01:26:25 +0100

    - removed lang

--- aboutme.php
+++ aboutme.php
@@ -2,7 +2,7 @@
 include 'include/db.php';
 ?>
 <!doctype html>
-<html lang="en">
+<html>
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
