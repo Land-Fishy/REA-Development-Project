541cc977285c6d0309f6ffb38d6abdb0c87e3403
cool cat <rapeb0ya@hotmail.com>
2020-02-21 13:12:19 +0100

    l

--- index.php
+++ index.php
@@ -21,18 +21,6 @@ if(true){
     <title>Lewis Wolfe</title>
     
     <link rel="stylesheet" type="text/css" href="css/stylesheet.css" important>
-	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-
-    <?php echo makeJsonld($db, $bookids); ?>
-  </head>
-  <body>
-    <div class="container" id="index">
-		  <?php require_once 'include/header.php';?>
-		  <main>
-        <?php echo makeBookFigures($db, $bookids); ?>
-		  </main>
-		  <?php require_once 'include/footer.php';?>
 	<link rel="stylesheet" type="text/css" href="css/botbar.css" important>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -64,7 +52,6 @@ if(true){
 		</main>
 		<?php require_once 'include/notification.php';?>
 		<?php require_once 'include/footer.php';?>
->>>>>>> 3e4bd13af8a59e2abe2f81bdd71767b213f0d1ba
     </div>
   </body>
 </html>
