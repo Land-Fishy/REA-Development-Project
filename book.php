413052cf67fe444d52cdfa69ba6bc93934d0e33a
Jermain <jermain@aboveinfinity.com>
2020-03-01 17:02:23 +0100

    merge

--- book.php
+++ book.php
@@ -19,6 +19,41 @@ include 'include/db.php';
         <h1 class="title">a monster escapes</h1>
         <div class="row">
           <iframe type="text/html" width="504" height="825" frameborder="0" allowfullscreen style="max-width:100%" src="https://read.amazon.com/kp/card?asin=B07WPKY3H1&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_-sYtEb2YBFR85&hideShare=true" ></iframe>
+            <table>
+              <tr>
+                <th>a monster escapes</th>
+              </tr>
+              <tr>
+                <td colspan="2" class="right">Author:</td>
+                <td>Lewis Wolfe</td>
+              </tr>
+              <tr>
+                <td colspan="2" class="right">Title:</td>
+                <td>A Monster Escapes</td>
+              </tr>
+              <tr>
+                <td colspan="2" class="right">Release Date:</td>
+                <td>August 23, 2019</td>
+              </tr>
+              <tr>
+                <td colspan="2" class="right">Genre:</td>
+                <td>Horror</td>
+              </tr>
+              <tr>
+                <td colspan="2" class="right">Description:</td>
+                <td>A dark force plagues the town of Brettville, Alabama, clawing at its victims and sending them into states of delusion, fear and paranoia. It looks for guilt and grief, trauma that smells and tastes so very sweet. While feeding on these painful memories, it lures its victims into a trap of endless waking nightmares.
+
+                Ex-marine Caleb Epps takes on the job of bodyguard for the strange investigator, Jane Elring. Together they travel to Brettville to unravel the mystery that holds the town in its suffocating grip. Soon Caleb finds himself caught up in a battle of wits between this strange, dark force and his new employer.
+
+                An employer that perhaps can't be trusted. As time goes on old questions make room for new ones. Why are the people that know Jane Elring afraid of her? What secrets lie hidden beneath the calm of her angelic face? And how does Caleb fit into the plans her sharp eyes suggest she has for him?
+
+                Monsters come in all shapes and sizes. Sometimes they're human.</td>
+              </tr>
+              <tr>
+                <td colspan="2" class="right">Language:</td>
+                <td>English</td>
+              </tr>
+            </table>
         </div>
        </div>
     </div>
