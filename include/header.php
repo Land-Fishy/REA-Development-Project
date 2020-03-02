8219188ef11e7655da2fccef005ba0ecf0cbe0db
Wieke Lemmens <wlemmens@reacollege.eu>
2020-03-02 15:51:01 +0100

    triangle

--- include/header.php
+++ include/header.php
@@ -1,29 +1,32 @@
-<header> 
-	<h1>Lewis Wolfe</h1>
-	<?php require_once 'include/nav.php';?>
+<header>
+	<div class="headerTop">
+		<h1>Lewis Wolfe</h1>
+		<?php require_once 'include/nav.php';?>
 
-	<div class="triangle"></div>
-<!-- Begin Mailchimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-<div id="mc_embed_signup">
-<form action="https://lewiswolfe.us3.list-manage.com/subscribe/post?u=9aa14dc4a67399e4bdcf46cc3&amp;id=0ed79b29fe" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<label for="mce-EMAIL">Subscribe to my newsletter</label>
-	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9aa14dc4a67399e4bdcf46cc3_0ed79b29fe" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>
+		<!-- Begin Mailchimp Signup Form -->
+		<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+		<div id="mc_embed_signup">
+		<form action="https://lewiswolfe.us3.list-manage.com/subscribe/post?u=9aa14dc4a67399e4bdcf46cc3&amp;id=0ed79b29fe" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<div id="mc_embed_signup_scroll">
+			<label for="mce-EMAIL">Subscribe to my newsletter</label>
+			<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+			<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9aa14dc4a67399e4bdcf46cc3_0ed79b29fe" tabindex="-1" value=""></div>
+			<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+			</div>
+		</form>
+		</div>
+	</div>
 
 <!--End mc_embed_signup-->
-	<div class="gradient"></div>
-	<div class="arrow">
-		<div></div>
-		<svg width="140" height="80">
+	<div class="headerBG">
+		<div class="gradient"></div>
+		<div class="arrow">
+			<div></div>
+			<svg width="140" height="80">
 			<polygon points="0,0 70,70 140,0 140,80 0,80" style="fill:rgb(238,238,238)" />
-		</svg>
-		<div></div>
+			</svg>
+			<div></div>
+		</div>
 	</div>
 </header>
