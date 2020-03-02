e3614c4732f1e92ffd564434c511856c4d32fc13
Casper Sloof <csloof@reacollege.eu>
2020-03-02 13:38:04 +0100

    meta added to includes

--- contact.php
+++ contact.php
@@ -6,11 +6,7 @@ include 'include/bookid.php';
 <!doctype html>
 <html>
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/stylesheet.css" important>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <title>Lewis Wolfe</title>
+	<?php require_once 'include/meta.php';?>
   </head>
   <body>
     <div class="container">
--- include/meta.php
+++ include/meta.php
@@ -1,3 +1,11 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Lewis Wolfe</title>
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css" important>
+<link rel="stylesheet" type="text/css" href="css/botbar.css" important>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<script src="script/botbar.js"></script>
+<?php echo makeJsonld($db, $bookids); ?>
 <meta name="theme-color" content="rgb(10,9,13)"/>
 <meta property="og:title" content="title"/>
 <meta property="og:type" content="type"/>
