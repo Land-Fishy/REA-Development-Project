4c852dd643d3d77ac5fdd1be8096187f15a0de65
Casper Sloof <csloof@reacollege.eu>
2020-03-02 15:12:58 +0100

    readded jquery

--- include/meta.php
+++ include/meta.php
@@ -4,6 +4,7 @@
 <link rel="stylesheet" type="text/css" href="css/stylesheet.css" important>
 <link rel="stylesheet" type="text/css" href="css/botbar.css" important>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="script/botbar.js"></script>
 <?php echo makeJsonld($db, $bookids); ?>
 <meta name="theme-color" content="rgb(10,9,13)"/>
