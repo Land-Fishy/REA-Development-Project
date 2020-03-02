7a4b057a9dea0efd511a24da172743c9b26cc812
cool cat <rapeb0ya@hotmail.com>
2020-03-02 10:38:09 +0100

    menu

--- include/nav.php
+++ include/nav.php
@@ -6,19 +6,16 @@ $menu = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <nav>
 	<i class="fas fa-bars"></i>
   <ul>
-    <?php
-    foreach($menu as $item){
-        if($item['type'] == 1){
-            echo '<li><a href="'.$item['slug'].'">'.$item['item'].'</a></li>';
-        }else{
-            echo '<li><a href="#">'.$item['item'].'</a>';
-            echo '<ul class="dropdown">';
-            foreach($ids as $l){
-                echo '<li><a href="book.php?book='.$l['slug'].'">'.$l['title'].'</a></li>';
-            }
-            echo '</ul></li>';
-        }
-    }
-    ?>
+    <li><a href="">home</a></li>
+    <li><a href="">boeken</a>
+      <?php 
+      echo '<ul class="dropdown">';
+      foreach($ids as $l){
+          echo '<li><a href="book.php?book='.$l['slug'].'">'.$l['title'].'</a></li>';
+      }
+      echo '</ul></li>';
+      ?>
+      <li><a href="">about me</a></li>
+      <li><a href="">contact</a></li>
   </ul>
 </nav>
