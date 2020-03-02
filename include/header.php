0b27bb71051a0caf8ffa567c62c965fda3ac2ff8
Jermain <jermain@aboveinfinity.com>
2020-03-02 12:22:44 +0100

    Added mailchimp

--- include/header.php
+++ include/header.php
@@ -1,7 +1,23 @@
 <header> 
 	<h1>Lewis Wolfe</h1>
 	<?php require_once 'include/nav.php';?>
+
 	<div class="triangle"></div>
+<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<div id="mc_embed_signup">
+<form action="https://lewiswolfe.us3.list-manage.com/subscribe/post?u=9aa14dc4a67399e4bdcf46cc3&amp;id=0ed79b29fe" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Subscribe to my newsletter</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9aa14dc4a67399e4bdcf46cc3_0ed79b29fe" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
 	<div class="gradient"></div>
 	<div class="arrow">
 		<div></div>
