014b3a5ce0b7893ffaeba7a72f79f8c87f59dd5a
Casper Sloof <csloof@reacollege.eu>
2020-03-03 15:42:01 +0100

    dynamic text

--- aboutme.php
+++ aboutme.php
@@ -16,13 +16,10 @@ include 'include/bookid.php';
         <h1 class="title">lewis wolfe</h1>
         <div class="row">
          <div class="about-me">
-          <p>Writing is a mysterious business. It's about diving deep into your own mind, sometimes your heart too, and putting whatever you find into words. Words that are put out into the world for all those interested to see. Stories, in this sense, are always a reflection of the person telling them.</p>
-
-          <p>There comes a time in your life when you will hear your calling. You'll either listen, or you won't. Lewis Wolfe heard his years ago and found himself unable to resist it. He had to write stories. Short ones and long ones. They were all just practice for his first novel, A Monster Escapes.</p>
-
-          <p>Lewis Wolfe believes that characters shape the story, never the other way around. Suspense and horror, then, are a natural extension of how people decide to lead their lives, be they real or imagined.</p>
-
-          <p>Consider following Lewis Wolfe on his journey as a storyteller. He would be honored.</p>
+		 <?php
+			$about = getAbout($db);
+			echo $about['text'];
+		 ?>
          </div>
          <img src="images/sexy.jpg" alt="Lewis Wolfe">
        </div>
