9afb356f4f67408c29879429d552d4f53d3c4ea1
Casper Sloof <csloof@reacollege.eu>
2020-03-06 10:20:32 +0100

    404 added

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>404 Error</title>
+		<link rel="icon" href="images/fav.png" sizes="36x36">
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css" important>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="container" id="E404">
+			<?php require_once 'include/nav.php';?>
+			<main>
+				<h1>404 NOT FOUND</h1>
+				<a href="http://www.lewiswolfe.com/new/">Go back</a>
+			</main>
+			<div class="footer">
+				<p>© Copyright - Lewis Wolfe</p>
+			</div>
+		</div>
+	</body>
+</html>
